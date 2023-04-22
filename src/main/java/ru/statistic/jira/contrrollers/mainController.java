9259604987415a78816b241ba6cc1ae93439bbfc
Калинин Александр <kalininalexjob@gmail.com>
2023-04-23 01:10:49 +0300

    Добавил в проект спринг web mvc и главную страницу

---
+++ src/main/java/ru/statistic/jira/contrrollers/mainController.java
@@ -0,0 +1,14 @@
+package ru.statistic.jira.contrrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title","Главная страница");
+        return "index";
+    }
+}
