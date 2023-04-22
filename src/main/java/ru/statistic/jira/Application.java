f21ad182eb33cc693a59528c35a5673c250e66c0
KalininAleksander <kalininalexjob@gmail.com>
2023-04-22 21:59:18 +0300

    Добавил в STDOUT явное начало старта

--- src/main/java/ru/statistic/jira/Application.java
+++ src/main/java/ru/statistic/jira/Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void  main(String[] args) {
+        System.out.println("Начало запуска приожения");
         SpringApplication.run(Application.class, args);
+        System.out.println("Модуль успешно запущен");
     }
-
 }
