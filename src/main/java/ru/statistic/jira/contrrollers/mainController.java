7a5c6d8f4258dd8d59c413a4b57c998a1889fdb9
Калинин Александр <kalininalexjob@gmail.com>
2023-04-28 00:07:37 +0300

    Добавил в проект примерный проект

--- src/main/java/ru/statistic/jira/contrrollers/mainController.java
+++ src/main/java/ru/statistic/jira/contrrollers/mainController.java
@@ -1,10 +1,12 @@
 package ru.statistic.jira.contrrollers;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@ComponentScan
 public class mainController {
     @GetMapping("/")
     public String index(Model model) {
